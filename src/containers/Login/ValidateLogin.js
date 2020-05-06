1fb213bb657f948ca8e43f6f67203e22bdfbe76a
dvshah26 <46349256+dvshah26@users.noreply.github.com>
2020-05-06 20:59:03 +0530

    add validations

--- src/containers/Login/ValidateLogin.js
+++ src/containers/Login/ValidateLogin.js
@@ -17,18 +17,24 @@ export function ValidateInput(data) {
     errors.password = "Password is required";
   }
   if (data.password !== undefined && !validator.isEmpty(data.password)) {
-    // if (
-    //   data.password !== undefined &&
-    //   validator.matches(data.password, regx) === false
-    // ) {
-    //   errors.password = ;
-    // }
     if (
       data.password !== undefined &&
       validator.isLength(data.password, { min: 6 }) === false
     ) {
-      errors.password =  "Passowrd should be minimum of 6 charcter";
+      errors.password = "Passowrd should be minimum of 6 charcter";
+    } else {
+      var pattern = new RegExp(
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{6,19}$/
+      );
+      if (
+        data.password !== undefined &&
+        pattern.test(data.password) === false
+      ) {
+        errors.password =
+          "Password must include at least a number, an uppercase and a lowercase letter";
+      }
     }
+    //  if(validator.matches()===false)
   }
   return { errors, isValid: isEmpty(errors) };
 }
--- src/containers/Register/ValidateRegister.js
+++ src/containers/Register/ValidateRegister.js
@@ -3,9 +3,8 @@ import isEmpty from "lodash/isEmpty";
 
 export function ValidateInput(data) {
   let errors = {};
- 
 
-  console.log("Email",data)
+  console.log("Email", data);
   if (data.first_name !== undefined && validator.isEmpty(data.first_name)) {
     errors.first_name = "FirstName is required";
   }
@@ -16,7 +15,7 @@ export function ValidateInput(data) {
   if (data.email !== undefined && validator.isEmpty(data.email)) {
     errors.email = "Email is required";
   }
-  if (data.email !== undefined && validator.isEmpty(data.email)===false) {
+  if (data.email !== undefined && validator.isEmpty(data.email) === false) {
     if (!validator.isEmail(data.email)) {
       errors.email = "Email is invalid";
     }
@@ -36,19 +35,30 @@ export function ValidateInput(data) {
       errors.phone = "Phone no should be of 10 digit";
     }
   }
- 
+
   if (data.password !== undefined && validator.isEmpty(data.password)) {
     errors.password = "Password is required";
   }
 
   if (data.password !== undefined && !validator.isEmpty(data.password)) {
-  
     if (
       data.password !== undefined &&
       validator.isLength(data.password, { min: 6 }) === false
     ) {
       errors.password = "Passowrd should be minimum of 6 charcter";
+    } else {
+      var pattern = new RegExp(
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{6,19}$/
+      );
+      if (
+        data.password !== undefined &&
+        pattern.test(data.password) === false
+      ) {
+        errors.password =
+          "Password must include at least a number, an uppercase and a lowercase letter";
+      }
     }
+    //  if(validator.matches()===false)
   }
   return { errors, isValid: isEmpty(errors) };
 }
